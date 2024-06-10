3fc4a6f9a71c99503690aaea95d2ba96145b05e1
Adam Standish <devadamstandish@gmail.com>
2024-06-10 13:33:31 -0400

    initial commit

---
+++ config/db.js
@@ -0,0 +1,30 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
+const env = import.meta.env;
+
+const firebaseConfig = {
+  apiKey: env.VITE_API_KEY,
+  
+  authDomain: env.VITE_API_URL,
+
+  projectId: env.VITE_API_PROJECT_ID,
+
+  storageBucket: env.VITE_API_STORAGE_BUCKET,
+
+  messagingSenderId: env.VITE_SENDER_ID,
+
+  appId: env.VITE_APP_ID,
+
+  measurementId: env.VITE_MEASUREMENT_ID,
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
