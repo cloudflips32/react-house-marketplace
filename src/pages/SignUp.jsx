55c7c7ab20a15abaeec9ea0fdd206c84cdd62a12
Adam Standish <devadamstandish@gmail.com>
2024-06-17 14:36:47 -0400

    signup firebase set

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,5 +1,7 @@
 import { useState } from "react"
 import { Link, useNavigate } from 'react-router-dom'
+import {getAuth, createUserWithEmailAndPassword, updateProfile } from 'firebase/auth'
+import {db} from '../config/firebase.config' 
 import ArrowRightIcon from '../assets/svg/keyboardArrowRightIcon.svg?react' 
 import VisibilityIcon from '../assets/svg/visibilityIcon.svg' 
 
@@ -24,6 +26,25 @@ const SignUp = () => {
     })
   }
 
+  const onSubmit = async (e) => {
+    e.preventDefault()
+
+    try {
+      const auth = getAuth()
+
+      const userCredential = await createUserWithEmailAndPassword(auth, email, password)
+      const user = userCredential.user
+
+      updateProfile(auth.currentUser, {
+        displayName: name
+      })
+
+      navigate('/')
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
   return (
     <>
       <div className="pageContainer">
@@ -31,7 +52,7 @@ const SignUp = () => {
           <p className="pageHeader">Welcome Back!</p>
         </header>
         <main>
-          <form>
+          <form onSubmit={onSubmit}>
             <input type="text" className="nameInput" placeholder="Name" id="name" value={name} 
             onChange={onChange} />
 
