73d3f556d27d3c1ebd80f895e61acd38ba12b50b
cloudflips32 <devadamstandish@gmail.com>
2024-10-28 22:21:08 -0400

    use client added

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
